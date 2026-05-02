8ec3380843edd9321ac3ae22f2275f7e24506343
lucas <66681646+limyeechern@users.noreply.github.com>
2026-05-02 18:24:32 +0800

    Add: minimum-path-sum - submission-4

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        costs = [[0 for i in range(cols + 1)] for j in range(rows + 1)]
+
+        for row in range(rows-1,-1,-1):
+            for col in range(cols-1,-1,-1):
+                currCost = grid[row][col]
+                costs[row][col] = grid[row][col]
+                if row == rows - 1 and col < cols - 1: # last row and still have more columns
+                    costs[row][col] = currCost + costs[row][col + 1]
+                elif col == cols - 1 and row < rows -1: # last column and still have more rows
+                    costs[row][col] = currCost + costs[row + 1][col]
+                else:
+                    costs[row][col] = currCost + min(costs[row][col + 1], costs[row + 1][col])
+        
+        return costs[0][0]
+                
+                
+
