bcc911704cfbf28dd77f2a495ea494daea4fa217
lucas <66681646+limyeechern@users.noreply.github.com>
2026-05-07 00:13:15 +0800

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache = {} # (index, total)
+
+        def dfs(index, total):
+            if total == target and index == len(nums):
+                return 1
+            if index >= len(nums):
+                return 0
+            if (index, total) in cache:
+                print("foo")
+                return cache[(index, total)]
+
+            add = dfs(index + 1, total + nums[index])
+            subtract = dfs(index + 1, total - nums[index])
+
+            cache[(index)] = add + subtract
+            return add + subtract
+
+        print(cache)
+        return dfs(0,0)
+
